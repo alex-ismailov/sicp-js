9433693d3dbdefdc7165dcc4d35b75671dbdd63a
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-19 00:23:18 +0300

    refactored: list fib squares function

--- src/02 build abstr-s with data/124 - list fib squares.js
+++ src/02 build abstr-s with data/124 - list fib squares.js
@@ -1,23 +1,25 @@
-import { toString as listToString } from '@hexlet/pairs-data';
-import {
-  lMap, enumerateInterval,
-} from '../../myLib/higherOrderFunctions';
+import { lMap, enumerateInterval, show } from '../../libs/seqlib';
 
 const fib = (n) => {
-  const iter = (a, b, count) => {
-    if (count === 0) {
-      return b;
-    }
-    return iter(a + b, a, count - 1);
-  };
+  const iter = (a, b, count) => (
+    count === 0
+      ? b
+      : iter(a + b, a, count - 1)
+  );
   return iter(1, 0, n);
 };
 
-const listFibSquares = (num) => {
-  const interval = enumerateInterval(0, num);
-  const fibs = lMap(fib, interval);
-  return lMap((n) => n ** 2, fibs);
-};
+const listFibSquares = (num) => (
+  lMap((curr) => curr ** 2,
+    lMap((n) => fib(n),
+      enumerateInterval(1, num)))
+);
+
+// const listFibSquares = (num) => {
+//   const interval = enumerateInterval(0, num);
+//   const fibs = lMap(fib, interval);
+//   return lMap((n) => n ** 2, fibs);
+// };
 
 /* testing */
-console.log(listToString(listFibSquares(10)));
+show(listFibSquares(7));
