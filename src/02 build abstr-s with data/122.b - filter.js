db266111a05a9ddf2c013485c4ff946f7f08a530
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-18 21:56:32 +0300

    refactored: lFilter function

--- src/02 build abstr-s with data/122.b - filter.js
+++ src/02 build abstr-s with data/122.b - filter.js
@@ -1,18 +1,35 @@
-import { 
+import {
   l, head, tail, isEmpty, toString as listToString, cons,
 } from '@hexlet/pairs-data';
 
-const lFilter = (predicate, sequence) => {
-  if (isEmpty(sequence)) {
+const lFilter = (fn, list) => {
+  if (isEmpty(list)) {
     return l();
   }
-  if (predicate(head(sequence))) {
-    return cons(head(sequence), lFilter(predicate, tail(sequence)));
-  }
-  return lFilter(predicate, tail(sequence));
+  return fn(head(list))
+    ? cons(head(list), lFilter(fn, tail(list)))
+    : lFilter(fn, tail(list));
 };
 
+// const lFilter = (fn, list) => (
+//   isEmpty(list)
+//     ? l()
+//     : fn(head(list))
+//       ? cons(head(list), lFilter(fn, tail(list)))
+//       : lFilter(fn, tail(list))
+// );
+
+// const lFilter = (predicate, sequence) => {
+//   if (isEmpty(sequence)) {
+//     return l();
+//   }
+//   if (predicate(head(sequence))) {
+//     return cons(head(sequence), lFilter(predicate, tail(sequence)));
+//   }
+//   return lFilter(predicate, tail(sequence));
+// };
+
 /* testing */
-const seq = l(1, 2, 3, 4, 5, 6)
+const seq = l(1, 2, 3, 4, 5, 6);
 console.log(listToString(lFilter((n) => n % 2 !== 0, seq)));
 console.log(listToString(lFilter((n) => n % 2 === 0, seq)));
