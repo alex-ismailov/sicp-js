693879b6942289aa2a0602abd2bfff0e429b5799
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-19 18:13:19 +0300

    added: ex 2.42 - 8 queens - but it is not work!

---
+++ src/02 build abstr-s with data/130 - ex 2.42 - 8 queens.js
@@ -0,0 +1,51 @@
+import { l, isEmpty } from "@hexlet/pairs-data";
+import { lFilter, flatMap, lMap, enumerateInterval, foldRight, show } from "../../libs/seqlib";
+
+// const queens = (boardSize) => {
+//   const queenCols = (k) => {
+//     if (k === 0) {
+//       return l(emptyBoard)
+//     }
+//     return lFilter((positions) => safeIt(k, positions), 
+//       flatMap((restOfQueens) => lMap((newRow) => adjoinPos(newRow, k, restOfQueens),
+//         enumerateInterval(1, boardSize)),
+//         queenCols(k - 1)));
+//   };
+//   return queenCols(boardSize);
+// };
+
+const adjoinPos = (row, col, rest) => (
+  cons(cons(row, col), rest)
+);
+
+const emptyBoard = l();
+
+const isSafe = (k, positions) => {
+  const firstRow = head(head(positions));
+  const firstCol = tail(head(positions));
+  return foldRight((pos, soFar) => {
+    const row = head(pos);
+    const col = tail(pos);
+    return soFar &&
+      firstRow - firstCol !==
+      row - col &&
+      firstRow + firstCol !==
+      row + col &&
+      firstRow !== row; 
+  }, true, tail(positions))
+};
+
+const queens = (boardSize) => {
+  const queenCols = (k) => (
+    k === 0
+      ? l(emptyBoard)
+      : lFilter((positions) => isSafe(k, positions),
+          flatMap((restOfQueens) => lMap((newRow) => adjoinPos(newRow, k, restOfQueens),
+            enumerateInterval(1, boardSize)),
+          queenCols(k - 1)))
+  );
+  return queenCols(boardSize);
+};
+
+// show(queens(l(8, 8))); It is not work !!!!
+
