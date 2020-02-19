825ae7923b7ac4f69b39ba2597b40e5c8cce0dfc
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-19 12:10:09 +0300

    refactored: ex 2.39 - reverse (foldLeft-Right) functions

--- src/02 build abstr-s with data/128 - ex 2.39 - reverse (foldLeft-Right).js
+++ src/02 build abstr-s with data/128 - ex 2.39 - reverse (foldLeft-Right).js
@@ -1,7 +1,5 @@
-import {
-  l, isEmpty, head, tail, cons, concat, toString as listToString,
-} from '@hexlet/pairs-data';
-import { foldLeft, foldRight } from '../../myLib/higherOrderFunctions';
+import { l, cons, concat } from '@hexlet/pairs-data';
+import { foldLeft, foldRight, show } from '../../libs/seqlib';
 
 /* Упражнение 2.39.
 Закончите следующие определения reverse (упражнение 2.18) в терминах процедур
@@ -20,17 +18,27 @@ foldright и fold-left из упражнения 2.38:
 //   return fn(head(sequence), foldRight(fn, acc, tail(sequence)));
 // };
 
-/* fold-left */
-const reverse1 = (sequence) => {
-  return foldRight((curr, acc) => concat(acc, l(curr)), l(), sequence);
-};
+/* foldLeft reverse */
+const reverse1 = (seq) => (
+  foldLeft(cons, l(), seq)
+);
 
-/* fold-left */
-const reverse2 = (sequence) => {
-  return foldLeft((curr, acc) => cons(curr, acc), l(), sequence);
-};
+/* foldRight reverse */
+const reverse2 = (seq) => (
+  foldRight((curr, acc) => concat(acc, l(curr)), l(), seq)
+);
+
+// /* fold-left */
+// const reverse1 = (sequence) => {
+//   return foldRight((curr, acc) => concat(acc, l(curr)), l(), sequence);
+// };
+
+// /* fold-left */
+// const reverse2 = (sequence) => {
+//   return foldLeft((curr, acc) => cons(curr, acc), l(), sequence);
+// };
 
 /* testing */
 const list = l(1, 2, 3, 4, 5);
-console.log(listToString(reverse1(list)));
-console.log(listToString(reverse2(list)));
+show(reverse1(list));
+show(reverse2(list));
