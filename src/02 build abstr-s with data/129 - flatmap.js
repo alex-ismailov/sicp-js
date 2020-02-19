fbde23150ff9a3c03f4e4d55f79c2c9cfd9c27cf
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-19 13:10:42 +0300

    refactored: flatmap function

--- src/02 build abstr-s with data/129 - flatmap.js
+++ src/02 build abstr-s with data/129 - flatmap.js
@@ -1,14 +1,17 @@
 import {
   l, concat, toString as listToString,
 } from '@hexlet/pairs-data';
-import { foldRight, lMap, enumerateInterval } from '../../myLib/seqlib';
+import { foldRight, lMap, enumerateInterval, show } from '../../libs/seqlib';
 
-const flatMap = (fn, seq) => foldRight(concat, l(), lMap(fn, seq));
-
-const orderedPairs = flatMap(
-  (i) => lMap((j) => l(i, j), enumerateInterval(1, i - 1)),
-  enumerateInterval(1, 4),
+const flatMap = (cb, seq) => (
+  foldRight(concat, l(), lMap(cb, seq))
 );
 
+// const flatMap = (fn, seq) => foldRight(concat, l(), lMap(fn, seq));
+
 /* testing */
-console.log(listToString(orderedPairs));
+const pairs = (n) => flatMap((i) => lMap((j) => l(i, j),
+    enumerateInterval(1, i - 1)),
+  enumerateInterval(1, n - 1));
+
+show(pairs(4));
