37e79d64aecf11c5bc0ddfffa68d791ec8b7ca14
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-19 12:59:54 +0300

    refactored: 128 - makeSeqOfOrderedPairs function

--- src/02 build abstr-s with data/128 - makeSeqOfOrderedPairs.js
+++ src/02 build abstr-s with data/128 - makeSeqOfOrderedPairs.js
@@ -1,30 +1,44 @@
+import { l } from '@hexlet/pairs-data';
 import {
-  l, isEmpty, isList, head, tail, cons, concat, toString as listToString,
-} from '@hexlet/pairs-data';
-import { enumerateInterval, foldRight, lMap } from '../../myLib/seqlib';
+  enumerateInterval, foldRight, lMap, concat, show,
+} from '../../libs/seqlib';
 
 
 /* where 1 <= j < i <= n */
-const makeSeqOfOrderedPairs = (n) => foldRight(concat, l(), lMap((i) => {
-  return lMap((j) => l(i, j), enumerateInterval(1, i - 1));
-}, enumerateInterval(1, n)))
+const makeSeqOfOrderedPairs = (n) => (
+  foldRight(concat, l(),
+    lMap((i) => lMap((j) => l(i, j),
+      enumerateInterval(1, i - 1)),
+    enumerateInterval(1, n)))
+);
+
+// const makeSeqOfOrderedPairs = (n) => foldRight(concat, l(), lMap((i) => {
+//   return lMap((j) => l(i, j), enumerateInterval(1, i - 1));
+// }, enumerateInterval(1, n)));
 
 
 /* testing */
-console.log(listToString(makeSeqOfOrderedPairs(3)));
+show(makeSeqOfOrderedPairs(3));
 
 /* *************************************** */
 /* Разложим makeSeqOfOrderedPairs на состовляющие */
-const f = (n) => lMap((i) => {
-  return lMap((j) => l(i, j), enumerateInterval(1, i - 1));
-}, enumerateInterval(1, n));
+const f = (n) => (
+  lMap((i) => lMap((j) => l(i, j),
+    enumerateInterval(1, i - 1)),
+  enumerateInterval(1, n))
+);
+// ( (), ((2, 1)), ((3, 1), (3, 2)) )
+
+// const f = (n) => lMap((i) => {
+//   return lMap((j) => l(i, j), enumerateInterval(1, i - 1));
+// }, enumerateInterval(1, n)); // ( (), ((2, 1)), ((3, 1), (3, 2)) )
 
 /* 2й lMap строит подмножества на основе 1го множества
 кол-во подмножеств полученных из 2го lMap зависит от величины значения
 текущего элемента первого множества полученного из 1го lMap.
  __________________
 |  i  |  2  | 3  3 |
-|_____|_____|______| 
+|_____|_____|______|
 |  j  |  1  | 1  2 |
 |_____|_____|______|
 |i + j|(2,1)|(3, 1)|
@@ -32,7 +46,7 @@ const f = (n) => lMap((i) => {
 |_____|_____|______|
 
 l(1, 2, 3)
-| 
+|
 1 -> l()
 2 -> ((2, 1))
 3 -> ((3, 1), (3, 2))
@@ -41,11 +55,11 @@ l(1, 2, 3)
 
 const res = foldRight(concat, l(), f(3));
 
-console.log(listToString(f(3)));
-console.log(listToString(res));
+show(f(3));
+show(res);
 
 console.log('*****');
 /* Вот таким образом foldRight будет concat(ить) подмножества
 полученного множества подмножеств */
 const concated = concat(l(), concat(l(l(2, 1)), concat(l(l(3, 1), l(3, 2)), l('test'))));
-console.log(listToString(concated));
+show(concated);
