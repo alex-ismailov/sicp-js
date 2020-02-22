f8638a970c134db558257fe20ad7120c3579a6f2
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-22 22:33:04 +0300

    added: 156 - ex 2.59 union set.js

---
+++ src/02 build abstr-s with data/156 - ex 2.59 union set.js
@@ -0,0 +1,34 @@
+import { l, isEmpty, head, tail, cons } from "@hexlet/pairs-data";
+import { show } from "../../libs/seqlib";
+/* 
+Упражнение 2.59.
+Реализуйте операцию union-set для представления множеств в виде неупорядоченных списков.
+*/
+
+const isElementOfSet = (x, set) => {
+  if (isEmpty(set)) {
+    return false;
+  }
+  return x === head(set)
+    ? true
+    : isElementOfSet(x, tail(set))
+};
+
+const adjoinSet = (x, set) => (
+  isElementOfSet(x, set)
+    ? set
+    : cons(x, set)
+);
+
+const unionSet = (set1, set2) => (
+  isEmpty(set1)
+    ? set2
+    : adjoinSet(head(set1), unionSet(tail(set1), set2))
+);
+
+/* testing */
+console.log('*** unionSet ***');
+const set1 = l(1, 2, 5, 4, 3, 12);
+const set5 = l(12, 1, 15, 11);
+const setOfUnion = unionSet(set1, set5);
+show(setOfUnion);
