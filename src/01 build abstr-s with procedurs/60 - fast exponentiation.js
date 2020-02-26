f8af6a43f48ad20b3941eaf02eabd95b601ef824
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-26 20:54:22 +0300

    added: 60 - fast exponentiation.js

---
+++ src/01 build abstr-s with procedurs/60 - fast exponentiation.js
@@ -0,0 +1,19 @@
+const isEven = (n) => n % 2 === 0;
+
+/*
+b ** n = (b ** n/2) ** 2 -> если n четно
+b ** n = b · b ** n−1 -> если n нечетно
+*/
+
+const expt = (b, n) => {
+  if (n === 0) {
+    return 1;
+  }
+  return isEven(n)
+    ? expt(b, n / 2) ** 2
+    : b * expt(b, n - 1);
+};
+
+/* testing */
+console.log(expt(2, 5)); // 32
+console.log(expt(2, 11)); // 2048
