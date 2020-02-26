e68b6e0f6cccbfde39eb3d34f7061769e745d7d0
alex-ismailov <alexander.is.dev@gmail.com>
2020-02-26 17:06:52 +0300

    added: 59 - exponentiation (linear recursive).js

---
+++ src/01 build abstr-s with procedurs/59 - exponentiation (linear recursive).js
@@ -0,0 +1,11 @@
+/* linear recursive process */
+const expt = (n, e) => (
+  e === 0
+    ? 1
+    : n * expt(n, e - 1)
+);
+
+/* testing */
+console.log(expt(3, 1)); // 3
+console.log(expt(2, 2)); // 4
+console.log(expt(2, 5)); // 32
