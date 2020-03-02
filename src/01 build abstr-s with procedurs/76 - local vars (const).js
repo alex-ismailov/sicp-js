8d595ce05b6e5c4373e67ff9f85a89163673ded1
alex-ismailov <alexander.is.dev@gmail.com>
2020-03-02 23:48:50 +0300

    added: 76 - local vars (const).js

--- src/01 build abstr-s with procedurs/76 - local vars (const).js
+++ src/01 build abstr-s with procedurs/76 - local vars (const).js
@@ -5,6 +5,9 @@ f(x, y) = x(1 + xy)**2 + y(1 − y) + (1 + xy)(1 − y)
   b = 1 − y
   f(x, y) = xa**2 + yb + ab */
 
+const square = (x) => x * x;
+
+/* ************************ */
 const fn = (x, y) => {
   const fnHelp = (a, b) => {
     return x * (a ** 2) + y * b + a * b;
@@ -18,6 +21,13 @@ const fn2 = (x, y) => {
   })(1 + (x * y), 1 - y);
 };
 
+const f = (x, y) => {
+  const a = 1 + (x * y);
+  const b = 1 - y;
+  return x * square(a) + y * b + a * b;
+};
+
 /* testing */
-console.log(fn(4, 4));
-console.log(fn2(4, 4));
+console.log(fn(2, 2));
+console.log(fn2(2, 2));
+console.log(f(2, 2));
