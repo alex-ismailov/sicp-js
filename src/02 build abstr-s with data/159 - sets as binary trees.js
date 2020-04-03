18d1e2f08bfe35e3998acb5777ef3a869893a345
smilenb <alexander.is.dev@gmail.com>
2020-04-03 21:47:22 +0300

    refactored(02 build abstr-s with data/159 - sets as binary trees)

--- src/02 build abstr-s with data/159 - sets as binary trees.js
+++ src/02 build abstr-s with data/159 - sets as binary trees.js
@@ -1,14 +1,14 @@
 import {
-  isEmpty, head, tail, l,
+  isEmpty, head, tail, l, toString as listToString
 } from '@hexlet/pairs-data';
-import {
-  entry, leftBranch, rightBranch, makeTree,
-} from '../../libs/binTreeLib';
+// import {
+//   entry, leftBranch, rightBranch, makeTree,
+// } from '../../libs/binTreeLib';
 import { show } from '../../libs/seqlib';
 
-const getEntry = (tree) => head(tree);
-const getLeftBranch = (tree) => head(tail(tree));
-const getRightBranch = (tree) => head(tail(tail(tree)));
+const entry = (tree) => head(tree);
+const leftBranch = (tree) => head(tail(tree));
+const rightBranch = (tree) => head(tail(tail(tree)));
 
 const makeTree = (entry, leftBranch, rightBranch) => (
   l(entry, leftBranch, rightBranch)
@@ -18,28 +18,30 @@ const isElementOfSet = (x, set) => {
   if (isEmpty(set)) {
     return false;
   }
-  if (x === getEntry(set)) {
+  if (x === entry(set)) {
     return true;
   }
-  return x < getEntry(set)
-    ? isElementOfSet(x, getLeftBranch(set))
-    : isElementOfSet(x, getRightBranch(set));
+  return x < entry(set)
+    ? isElementOfSet(x, leftBranch(set))
+    : isElementOfSet(x, rightBranch(set));
 };
 
 const adjoinSet = (x, set) => {
   if (isEmpty(set)) {
     return makeTree(x, l(), l());
   }
-  if (x === getEntry(set)) {
+  if (x === entry(set)) {
     return true;
   }
-  return x < getEntry(set)
-    ? makeTree(getEntry(set), adjoinSet(x, getLeftBranch(set)), getRightBranch(set))
-    : makeTree(getEntry(set), getLeftBranch(set), adjoinSet(x, getRightBranch(set)))
+  return x < entry(set)
+    ? makeTree(entry(set), adjoinSet(x, leftBranch(set)), rightBranch(set))
+    : makeTree(entry(set), leftBranch(set), adjoinSet(x, rightBranch(set)))
 };
 
 /* testing */
+// const binTree1 = makeTree(5, makeTree(3, makeTree(1, l(), l()), l()), makeTree(9, makeTree(7, l(), l()), makeTree(11, l(), l())));
 const binTree1 = makeTree(5, makeTree(3, makeTree(1, l(), l()), l()), makeTree(9, makeTree(7, l(), l()), makeTree(11, l(), l())));
+console.log(listToString(binTree1));
 console.log('****** isElementOf Set *******');
 console.log(`isElementOfSet(11, binTree1) = ${isElementOfSet(11, binTree1)}`); // true
 console.log(`isElementOfSet(4, binTree1) = ${isElementOfSet(4, binTree1)}`); // false
