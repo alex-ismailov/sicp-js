9b065f000110aa001ed5c73eeaf2b0d4b70b7f3e
smilenb <alexander.is.dev@gmail.com>
2020-04-10 15:46:59 +0300

    refactor(02 build abstr-s with data/161 - ex 2.64 list to bal-tree.js) dev version

--- src/02 build abstr-s with data/161 - ex 2.64 list to bal-tree.js
+++ src/02 build abstr-s with data/161 - ex 2.64 list to bal-tree.js
@@ -7,9 +7,9 @@ car которой есть построенное дерево, а cdr — сп
 import { l, head, tail, cons, length } from "@hexlet/pairs-data";
 import { show } from "../../libs/seqlib";
 
-const entry = (tree) => head(tree);
-const leftBranch = (tree) => head(tail(tree));
-const rightBranch = (tree) => head(tail(tail(tree)));
+// const entry = (tree) => head(tree);
+// const leftBranch = (tree) => head(tail(tree));
+// const rightBranch = (tree) => head(tail(tail(tree)));
 
 const makeTree = (entry, leftBranch, rightBranch) => (
   l(entry, leftBranch, rightBranch)
@@ -17,6 +17,7 @@ const makeTree = (entry, leftBranch, rightBranch) => (
 
 /* ********************************************* */
 const listToTree = (elements) => {
+  // show(tail(partialTree(elements, length(elements))));
   return head(partialTree(elements, length(elements)));
 };
 
@@ -26,18 +27,55 @@ const partialTree = (elts, n) => {
   } else {
     const leftSize = Math.floor((n - 1) / 2);
     const leftResult = partialTree(elts, leftSize);
+    console.log('*****');
+    show('leftResult');
+    show(leftResult);
     const leftTree = head(leftResult);
+    show('leftTree');
+    show(leftTree);
     const nonLeftElts = tail(leftResult);
+    show('nonLeftElts');
+    show(nonLeftElts);
 
     const rightSize = n - (leftSize + 1);
     const thisEntry = head(nonLeftElts);
+    show('thisEntry');
+    show(thisEntry);
     const rightResult = partialTree(tail(nonLeftElts), rightSize);
+    show('rightResult');
+    show(rightResult);
     const rightTree = head(rightResult);
+    show('rightTree');
+    show(rightTree);
     const remainingElts = tail(rightResult);
+    show('remainingElts');
+    show(remainingElts);
+    show('makeTree');
+    show(makeTree(thisEntry, leftTree, rightTree));
+    console.log('*****');
 
     return cons(makeTree(thisEntry, leftTree, rightTree), remainingElts);
   }
 };
+
+// const partialTree = (elts, n) => {
+//   if (n === 0) {
+//     return cons(l(), elts);
+//   } else {
+//     const leftSize = Math.floor((n - 1) / 2);
+//     const leftResult = partialTree(elts, leftSize);
+//     const leftTree = head(leftResult); 
+//     const nonLeftElts = tail(leftResult);
+
+//     const rightSize = n - (leftSize + 1);
+//     const thisEntry = head(nonLeftElts);
+//     const rightResult = partialTree(tail(nonLeftElts), rightSize);
+//     const rightTree = head(rightResult);
+//     const remainingElts = tail(rightResult);
+
+//     return cons(makeTree(thisEntry, leftTree, rightTree), remainingElts);
+//   }
+// };
 
 /* testing */
 const list = l(1, 2, 3);
@@ -46,3 +84,8 @@ show(listToTree(list));
 
 // show(head(l()));
 // show(cons(l(), l(1, 2, 3)))
+// show(head(cons(l(), l(3))));
+// show(tail(l(1, l(), l())));
+
+// show(makeTree(1, l(), undefined));
+// show(tail(l(3)));
