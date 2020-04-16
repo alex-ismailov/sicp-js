570e3e23b0fa2f7ede5e2aa5f8b1bd8eca4ca044
smilenb <alexander.is.dev@gmail.com>
2020-04-16 23:50:14 +0300

    add(src/02 build abstr-s with data/) 168 - sets of weighted elements.js

---
+++ src/02 build abstr-s with data/168 - sets of weighted elements.js
@@ -0,0 +1,34 @@
+/* Мы представим множество листьев и деревьев как список элементов, упорядоченный
+по весу в возрастающем порядке. */
+
+import { l, cons, head, tail, isEmpty } from "@hexlet/pairs-data";
+
+import { getWeight, makeLeaf } from './166 - Huffman trees interface.js';
+import { show } from '../../libs/utils.js';
+
+const adjoinSet = (x, set) => {
+  if (isEmpty(set)) {
+    return l(x);
+  }
+  return getWeight(x) < getWeight(head(set))
+    ? cons(x, set)
+    : cons(head(set), adjoinSet(x, tail(set)));
+};
+
+const makeLeafSet = (pairs) => {
+  if (isEmpty(pairs)) {
+    return l();
+  }
+  const pair = head(pairs);
+
+  return adjoinSet(
+    makeLeaf(head(pair), tail(pair)),
+    makeLeafSet(tail(pairs)),  
+  );
+};
+
+/* testing */
+// const data = l(l('A', 4), l('B', 2), l('C', 1), l('D', 1));
+// show(data); // => ((A, 4), (B, 2), (C, 1), (D, 1))
+// show(makeLeafSet(data)); // => ((leaf, D, (1)), (leaf, C, (1)), (leaf, B, (2)), (leaf, A, (4)))
+
