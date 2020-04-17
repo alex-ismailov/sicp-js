2fb3328d8e7451605c9ecc1881db5cc4e83d98e9
smilenb <alexander.is.dev@gmail.com>
2020-04-17 11:36:21 +0300

    add(src/02 build abstr-s with data/) 169 - ex 2.68 - encode.js

--- src/02 build abstr-s with data/169 - ex 2.68 - encode.js
+++ src/02 build abstr-s with data/169 - ex 2.68 - encode.js
@@ -15,10 +15,14 @@
 Проверьте свою процедуру, закодировав тот результат, который Вы получили в упражнении 2.67,
 с деревом-примером и проверив, совпадает ли то, что получаете Вы, с исходным сообщением. */
 
-import { isEmpty, concat, head, cons } from "@hexlet/pairs-data"
-import { getSymbols, getLeftBranch } from "./166 - Huffman trees interface";
-
-
+/* eslint indent: 0 */
+import {
+ l, isEmpty, concat, head, tail, cons,
+} from '@hexlet/pairs-data';
+import { show } from '../../libs/utils';
+import {
+  makeCodeTree, makeLeaf, isLeaf, getSymbols, getLeftBranch, getRightBranch,
+} from './166 - Huffman trees interface';
 
 const isElementOfSet = (x, set) => {
   if (isEmpty(set)) {
@@ -26,7 +30,7 @@ const isElementOfSet = (x, set) => {
   }
   return x === head(set)
     ? true
-    : isElementOfSet(x, tail(set))
+    : isElementOfSet(x, tail(set));
 };
 
 const encodeSymbol = (symbol, tree) => {
@@ -34,17 +38,20 @@ const encodeSymbol = (symbol, tree) => {
     return l();
   }
   if (isElementOfSet(symbol, getSymbols(getLeftBranch(tree)))) {
-    return cons(0, encodeSymbol(symbol));
+    return cons(0, encodeSymbol(symbol, getLeftBranch(tree)));
   }
+  if (isElementOfSet(symbol, getSymbols(getRightBranch(tree)))) {
+    return cons(1, encodeSymbol(symbol, getRightBranch(tree)));
+  }
+  throw new Error(`плохой бит -- CHOOSE-BRANCH: '${symbol}'!`);
 };
 
 /* ************************************* */
 const encode = (message, tree) => (
-  isEmpty(tree)
+  isEmpty(message)
     ? l()
-    : concat(encodeSymbol(head(message), tree), encode(tail(message, tree)))
+    : concat(encodeSymbol(head(message), tree), encode(tail(message), tree))
 );
-
 
 /* testing */
 const sampleTree = makeCodeTree(makeLeaf('A', 4),
@@ -53,8 +60,23 @@ const sampleTree = makeCodeTree(makeLeaf('A', 4),
                       makeCodeTree(
                         makeLeaf('D', 1),
                         makeLeaf('C', 1),
-                      )));
+                      ),
+));
 
-const msg = l(A, D, A, B, B, C, A);
+const msgA = l('A');
+const msgB = l('B');
+const msgC = l('C');
+const msgD = l('D');
+const msg1 = l('A', 'D', 'A', 'B', 'B', 'C', 'A');
+// const msg3 = l('A', 'C', '/', 'D', 'C');
 
+show(encode(msgA, sampleTree)); // => (0)
+show(encode(msgB, sampleTree)); // => (1, 0)
+show(encode(msgD, sampleTree)); // => (1, 1, 0)
+show(encode(msgC, sampleTree)); // => (1, 1, 1)
 
+show(encode(msg1, sampleTree)); // => l(0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0);
+// (0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0)
+// (0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0)
+
+// show(encode(msg3, sampleTree)); // => throw Error: плохой бит -- CHOOSE-BRANCH: '/'!
